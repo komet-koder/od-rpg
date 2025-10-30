39a844ecf0be1c7c4097394e99390476a4611d17
kawillrich <kurt.a.willrich@gmail.com>
2025-10-29 20:42:02 -0400

    feat: update mage spell level 3 containers  and styling

--- js/js_v14-3.js
+++ js/js_v14-3.js
@@ -3606,17 +3606,21 @@ function populateMageSpells(m1, m2, nextChap) {
       spellList.push(characterThirdLevelSpells[i].name);
       let spellLi = document.createElement("ul");
       spellLi.classList.add(`spell-list-combat-${characterThirdLevelSpells[i].className}`);
+      
       spellLi.textContent = characterThirdLevelSpells[i].name;
-      // let addedSpellName = document.createElement("span");
-      // addedSpellName.innerHTML = characterThirdLevelSpells[i].name;
-      // spellLi.appendChild(addedSpellName);
+
+     let targetButtonDiv = document.createElement('div');
+      targetButtonDiv.setAttribute('class', 'spell-target-div');
+      spellLi.appendChild(targetButtonDiv);
+
       let addedSpellUl = document.querySelector(".third-level-spell-list-container");
       addedSpellUl.appendChild(spellLi);
 
       let addSelf1Btn = document.createElement("li");
       addSelf1Btn.classList.add("self-spell-list");
       addSelf1Btn.innerText = "Self";
-      spellLi.appendChild(addSelf1Btn);
+      targetButtonDiv.appendChild(addSelf1Btn);
+
       addSelf1Btn.addEventListener(
         "click",
         function (e) {
@@ -3628,19 +3632,26 @@ function populateMageSpells(m1, m2, nextChap) {
       spellList.push(characterThirdLevelSpells[i].name);
       let spellLi = document.createElement("ul");
       spellLi.classList.add(`spell-list-combat-${characterThirdLevelSpells[i].className}`);
-      let addedSpellName = document.createElement("span");
-      addedSpellName.innerHTML = characterThirdLevelSpells[i].name;
-      spellLi.appendChild(addedSpellName);
+      
+      spellLi.textContent = characterThirdLevelSpells[i].name;
+
+      let targetButtonDiv = document.createElement('div');
+      targetButtonDiv.setAttribute('class', 'spell-target-div');
+      spellLi.appendChild(targetButtonDiv);
+
       let addedSpellUl = document.querySelector(".third-level-spell-list-container");
       addedSpellUl.appendChild(spellLi);
+
       let addMonster1Btn = document.createElement("li");
       addMonster1Btn.classList.add("monster-one-spell-list");
       addMonster1Btn.innerText = "Monster 1";
-      spellLi.appendChild(addMonster1Btn);
+      targetButtonDiv.appendChild(addMonster1Btn);
+
       let addMonster2Btn = document.createElement("li");
       addMonster2Btn.classList.add("monster-two-spell-list");
       addMonster2Btn.innerText = "Monster 2";
-      spellLi.appendChild(addMonster2Btn);
+      targetButtonDiv.appendChild(addMonster2Btn);
+      
       addMonster1Btn.addEventListener(
         "click",
         function (e) {
