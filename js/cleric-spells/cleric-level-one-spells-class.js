9bbc1b354ebbd8f1e93e7562a04e0fd39e4d6b44
kawillrich <kurt.a.willrich@gmail.com>
2025-11-15 19:45:04 -0500

    refactor: replace clericLight with mage's Light spell

--- js/cleric-spells/cleric-level-one-spells-class.js
+++ js/cleric-spells/cleric-level-one-spells-class.js
@@ -131,119 +131,103 @@ cureLightWoundsCleric.castSpell = function (monster1, monster2, continueNextChap
 lightCleric.castSpell = function (monster1, monster2, continueNextChapter, attackedMonster)
 {
     let dialogue = document.querySelector('#fight-module-dialogue');
-    if (this.numberOfUses <= 0)
-    {
-        let dialogue = document.querySelector('#fight-module-dialogue');
-        dialogue.innerHTML = `<p>You try to cast Light, but the words won't come to your mind.</p>`;
-        toggleShowSpellList();
-
-    } else
-    {
-        this.numberOfUses -= 1;
-
         if (this.numberOfUses <= 0)
         {
-            this.numberOfUses = 0;
-        }
-        toggleShowSpellList();
-
-        finalCharacter.greyOutAttackButtons(monster1, monster2);
-        dialogue.innerHTML = `<p>You cast Light, which affects a 30 foot area.</p>`;
-
-        let isBlindedM1 = monster1.status.some((x) => x === "Blind");
-        let isBlindedM2 = monster2.status.some((x) => x === "Blind");
-
-        //checking if M1 is alive -
-
-        if ((attackedMonster === "Monster 1" && monster1.healthPoints > 0 && isBlindedM1 === false) || (attackedMonster === "Monster 1" && (monster1.healthPoints > 0 && isBlindedM1 === false)))
+            SpellUtilities.cantCastSpell("Light", dialogue);
+            toggleShowSpellList();
+    
+        } else
         {
-            dialogue.innerHTML += `<p>${monster1.name} has been blinded.</p>`;
-            monster1.status.push('Blind');
-            let monster1Status = document.querySelector("#monster-one-status");
-            monster1Status.innerHTML = `<h4 id="monster-one-status">Status: ${monster1.status.join(', ')}</h4>`
-
-            //need to make variable, push to an array, and then call the function expression
-
-            let blindTimer = setTimeout(function ()
+            this.numberOfUses -= 1;
+    
+            if (this.numberOfUses <= 0)
             {
-                // let removeBlindM1 = monster1.status.filter((x) => "Blind");
-                // monster1.status.splice(monster1.status.indexOf("Blind"), 1);
-                // monster1.status.splice(removeBlindM1); //removing Blind after function call
-                monster1.status.splice(monster1.status.indexOf("Blind"), 1);
-
-                let updateM1Status = document.querySelector("#monster-one");
-                updateM1Status.innerHTML = `
-                <div class="monster" id="monster-one">
-                    <fieldset class='monster-info-module'>
-                        <legend class='monster-dashboard'>Monster 1</legend>
-                        <h4 id="monster-one-type">Monster Type: ${monster1.name}</h4>
-                        <h4 id="monster-one-hp">Hit Points: ${monster1.healthPoints}<progress class='monster-hp-prog-bar'  id="monster-one-hp-progress-bar" max="${monster1.startingHealthPoints}" value="${monster1.healthPoints}"></progress></h4> 
-                        <h4 id="monster-one-ap">Armor Class: ${monster1.armorClass}</h4>
-                        <h4 id="monster-one-damage">Damage: ${monster1.damage}</h4>
-                        <h4 id="monster-one-status">Status: ${monster1.status}</h4>
-                    </fieldset>   
-                </div>`;
-                console.log('Blind removed m1')
-            }, 30000);
-
-            finalCharacter.activeSpellStatuses.push(blindTimer);
-
-            let blindTimerObject = {
-                id: blindTimer,
-                name: "Light Timer 1"
-            };
-
-            finalCharacter.activeSpellStatuses.push(blindTimerObject);
-            console.log(finalCharacter.activeSpellStatuses)
-
-            //end of setTimeout
-
-        } else if ((attackedMonster === "Monster 2" && monster2.healthPoints > 0 && isBlindedM2 === false) || (attackedMonster === "Monster 2" && (monster2.healthPoints > 0 && isBlindedM2 === false)))
-        {
-            dialogue.innerHTML += `<p>${monster2.name} is blinded.</p>`;
-            monster2.status.push('Blind');
-            let monster2Status = document.querySelector("#monster-two-status");
-            monster2Status.innerHTML = `<h4 id="monster-two-status">Status: ${monster2.status.join(', ')}</h4>`
-
-            let blindTimer2 = setTimeout(function ()
+                this.numberOfUses = 0;
+            }
+            toggleShowSpellList();
+    
+            finalCharacter.greyOutAttackButtons(monster1, monster2);
+            dialogue.innerHTML = `<p>You cast Light, which affects a 30 foot area.</p>`;
+    
+            let isBlindedM1 = monster1.status.some((x) => x === "Blind");
+            let isBlindedM2 = monster2.status.some((x) => x === "Blind");
+    
+            //checking if M1 is alive -
+    
+            if ((attackedMonster === "Monster 1" && monster1.healthPoints > 0 && isBlindedM1 === false) || (attackedMonster === "Monster 1" && (monster1.healthPoints > 0 && isBlindedM1 === false)))
             {
-                monster2.status.splice(monster2.status.indexOf("Blind"), 1);
-                // let removeBlindM2 = monster2.status.filter((x) => "Blind");
-                // monster2.status.splice(removeBlindM2);
-                let updateM2Status = document.querySelector("#monster-two");
-                updateM2Status.innerHTML = `
-                <div class="monster" id="monster-two">
-                    <fieldset class='monster-info-module'>
-                        <legend class='monster-dashboard'>Monster 2</legend>
-                        <h4 id="monster-two-type">Monster Type: ${monster2.name}</h4>
-                        <h4 id="monster-two-hp">Hit Points: ${monster2.healthPoints}<progress class='monster-hp-prog-bar'  id="monster-two-hp-progress-bar" max="${monster2.startingHealthPoints}" value="${monster2.healthPoints}"></progress></h4> 
-                        <h4 id="monster-two-ap">Armor Class: ${monster2.armorClass}</h4>
-                        <h4 id="monster-two-damage">Damage: ${monster2.damage}</h4>
-                        <h4 id="monster-two-status">Status: ${monster2.status}</h4>
-                    </fieldset>   
-                </div>`;
-                console.log('blind removed m2')
-
-            }, 30000);
-
-            finalCharacter.activeSpellStatuses.push(blindTimer2);
-            let blindTimerObject2 = {
-                id: blindTimer2,
-                name: "Light Timer 2"
-            };
-
-            finalCharacter.activeSpellStatuses.push(blindTimerObject2);
-
-            console.log(finalCharacter.activeSpellStatuses)
-
-        } else if (attackedMonster === "Monster 2")
-        {
-            dialogue.innerHTML += `<p>${monster2.name} was not affected.</p>`;
-        } else if (attackedMonster === "Monster 1")
-        {
-            dialogue.innerHTML += `<p>${monster1.name} was not affected.</p>`;
+                dialogue.innerHTML += `<p>${monster1.name} has been blinded.</p>`;
+                monster1.status.push('Blind');
+    
+                let monster1Status = document.querySelector("#monster-one-status");
+                monster1Status.innerHTML = `${monster1.status.join(', ')}`
+    
+                //need to make variable, push to an array, and then call the function expression
+    
+                let blindTimer = setTimeout(function ()
+                {
+                    
+                    monster1.status.splice(monster1.status.indexOf("Blind"), 1);
+    
+                    Utilities.updateMonsterOne(monster1);
+                    
+                    let monster1Status = document.querySelector("#monster-one-status");
+                    monster1Status.innerHTML = `${monster1.status.join(', ')}`;
+    
+                    console.log('Blind removed m1')
+                }, 30000);
+    
+                finalCharacter.activeSpellStatuses.push(blindTimer);
+    
+                let blindTimerObject = {
+                    id: blindTimer,
+                    name: "Light Timer 1"
+                };
+    
+                finalCharacter.activeSpellStatuses.push(blindTimerObject);
+                console.log(finalCharacter.activeSpellStatuses)
+    
+                //end of setTimeout
+    
+            } else if ((attackedMonster === "Monster 2" && monster2.healthPoints > 0 && isBlindedM2 === false) || (attackedMonster === "Monster 2" && (monster2.healthPoints > 0 && isBlindedM2 === false)))
+            {
+                dialogue.innerHTML += `<p>${monster2.name} is blinded.</p>`;
+                monster2.status.push('Blind');
+                
+                let monster2Status = document.querySelector("#monster-two-status");
+                monster2Status.innerHTML = `${monster2.status.join(', ')}`
+    
+                let blindTimer2 = setTimeout(function ()
+                {
+                    monster2.status.splice(monster2.status.indexOf("Blind"), 1);
+                   
+                    Utilities.updateMonsterTwo(monster2);
+    
+                    let monsterTwoStatus = document.querySelector("#monster-two-status");
+                    monsterTwoStatus.innerHTML = `${monster2.status.join(', ')}`;                
+                    
+                    console.log('blind removed m2')
+    
+                }, 30000);
+    
+                finalCharacter.activeSpellStatuses.push(blindTimer2);
+                let blindTimerObject2 = {
+                    id: blindTimer2,
+                    name: "Light Timer 2"
+                };
+    
+                finalCharacter.activeSpellStatuses.push(blindTimerObject2);
+    
+                console.log(finalCharacter.activeSpellStatuses)
+    
+            } else if (attackedMonster === "Monster 2")
+            {
+                dialogue.innerHTML += `<p>${monster2.name} was not affected.</p>`;
+            } else if (attackedMonster === "Monster 1")
+            {
+                dialogue.innerHTML += `<p>${monster1.name} was not affected.</p>`;
+            }
         }
-    }
 }
 
 export { cureLightWoundsCleric, detectEvilCleric, detectMagicCleric, purifyFoodAndWaterCleric, lightCleric, removeFearCleric, protectionFromEvilCleric, resistColdCleric };
